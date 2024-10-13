e58e8e1cc1f7f2fae4d8cf287f148fe127cfe01e
Reda Bacha <redabacha01@gmail.com>
2024-10-14 00:23:39 +0100

    fix: make typechecking work correctly

--- app/env.d.ts
+++
@@ -1,4 +0,0 @@
-// This isn't declared by the @types/react-dom package but exists as an export of react-dom.
-declare module "react-dom/server.browser" {
-  export * from "react-dom/server";
-}
--- app/react.d.ts
+++ app/react.d.ts
@@ -4,3 +4,18 @@ declare module "react" {
   // @ts-expect-error export assignment :(
   export = React;
 }
+
+declare module "react-dom" {
+  // @ts-types="@types/react-dom"
+  export * from "npm:react-dom";
+}
+
+declare module "react-dom/client" {
+  // @ts-types="@types/react-dom/client"
+  export * from "npm:react-dom/client";
+}
+
+declare module "react-dom/server.browser" {
+  // @ts-types="@types/react-dom/server"
+  export * from "npm:react-dom/server";
+}
