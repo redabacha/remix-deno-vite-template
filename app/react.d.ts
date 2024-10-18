116fd00225f8e0248b851c1a21c98a1ee9e37b98
Reda Bacha <redabacha01@gmail.com>
2024-10-19 00:19:10 +0100

    fix: add versions to react.d.ts workaround

--- app/react.d.ts
+++ app/react.d.ts
@@ -1,21 +1,21 @@
 declare module "react" {
   // @ts-types="@types/react"
-  import React from "npm:react";
+  import React from "npm:react@^18.3.1";
   // @ts-expect-error export assignment :(
   export = React;
 }
 
 declare module "react-dom" {
   // @ts-types="@types/react-dom"
-  export * from "npm:react-dom";
+  export * from "npm:react-dom@^18.3.1";
 }
 
 declare module "react-dom/client" {
   // @ts-types="@types/react-dom/client"
-  export * from "npm:react-dom/client";
+  export * from "npm:react-dom@^18.3.1/client";
 }
 
 declare module "react-dom/server.browser" {
   // @ts-types="@types/react-dom/server"
-  export * from "npm:react-dom/server";
+  export * from "npm:react-dom@^18.3.1/server";
 }
